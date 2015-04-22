b7105e8ecf2b434610597ab8b6e94739b0a76ac4
Alister Bulman <abulman@gmail.com>
2015-04-22 13:33:49 +0100

    displaying all expenses on screen

--- src/AppBundle/Resources/public/js/expenses.js
+++ src/AppBundle/Resources/public/js/expenses.js
@@ -1,29 +1,20 @@
 /* expenses.js */
 
-var expensesApp = angular.module('expensesApp', [
-  'ngRoute',
-  //'expensesControllers'
-]);
+var expensesApp = angular.module('expensesApp', [ 'ngRoute', 'ngTable' ]);
 expensesApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/expenses', {
-        templateUrl: 'partials/demo.html',
+        templateUrl: '/partials/expense_list.html',
         controller: 'ExpensesCtrl',
         controllerAs: 'expenses'
       }).
       when('/about', {
         templateUrl: '/partials/about.html',
-        //controller: 'PhoneDetailCtrl'
       }).
       when('/alister', {
         templateUrl: '/partials/alister.html',
-        //controller: 'PhoneDetailCtrl'
       }).
-      // when('/expenses/:phoneId', {
-      //   templateUrl: 'partials/phone-detail.html',
-      //   controller: 'PhoneDetailCtrl'
-      // }).
 
       // go to /expenses by default
       otherwise({
@@ -33,11 +24,7 @@ expensesApp.config(['$routeProvider',
 
 expensesApp.controller('ExpensesCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.expenses = [];
-
-    //$http.get('/api/v1/demo/expenses.json').success(function(data) {
     $http.get('/api/v1/expenses.json').success(function(data) {
         $scope.expenses = data.expenses;
     });
-
-    $scope.orderProp = 'created_at';
 }]);
