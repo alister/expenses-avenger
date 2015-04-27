76f72554d11c866571cc93d8dd44b500da123cab
Alister Bulman <abulman@gmail.com>
2015-04-27 16:57:28 +0100

    logins by G+, via http://userapp.io with angularJS support

--- web/src/app.js
+++ web/src/app.js
@@ -1,4 +1,4 @@
-angular.module('ExpensesApp', ['ngRoute', 'ngResource', 'ngMessages', 'ui.bootstrap', 'ng-currency', 'datePicker']) // 'gm.datepickerMultiSelect', 
+angular.module('ExpensesApp', ['ngRoute', 'ngResource', 'ngMessages', 'ui.bootstrap', 'ng-currency', 'datePicker', 'UserApp'])
     .config(['$httpProvider', '$routeProvider', '$locationProvider', function ($httpProvider, $routeProvider, $locationProvider) {
         $routeProvider
             .when('/expenses', {
@@ -18,11 +18,20 @@ angular.module('ExpensesApp', ['ngRoute', 'ngResource', 'ngMessages', 'ui.bootst
                 templateUrl: 'views/single.html'
             })
             .when('/about', {
-                templateUrl: 'views/about.html'
+                templateUrl: 'views/about.html', public: true
             })
             .when('/alister', {
-                templateUrl: 'views/alister.html'
+                templateUrl: 'views/alister.html', public: true
             })
+
+            .when('/login', {templateUrl: 'partials/login.html', login: true})
+            .when('/signup', {templateUrl: 'partials/signup.html', public: true})
+            .when('/verify-email', {templateUrl: 'partials/verify-email.html', verify_email: true})
+            .when('/reset-password', {templateUrl: 'partials/reset-password.html', public: true})
+            .when('/set-password', {templateUrl: 'partials/set-password.html', set_password: true})
+            .when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'})
+            .when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'})
+
             .otherwise({
                 redirectTo: '/expenses'
             });
@@ -30,9 +39,10 @@ angular.module('ExpensesApp', ['ngRoute', 'ngResource', 'ngMessages', 'ui.bootst
         }
     ])
     .value('options', {})
-    .run(function ($rootScope, options, Expense) {
-        paramsObj= {};
-    });
+    .run(['user', function (user) { //$rootScope, options, Expense, 
+        paramsObj = {};
+        user.init({ appId: '553e4bb6566ea' });
+    }]);
 angular.module('ExpensesApp')
     .factory('Expense', function ($resource) {
         return $resource('/api/v1/expenses/:id.json',  //http://nas.abulman.co.uk:8000/app_dev.php
